c7f49a75fbcd80d3cc236f612a8907f53de9e2dd
Mezerg <j.kwiatkowski122@gmail.com>
2024-01-16 18:25:28 +0100

    Dodanie sekcji contact

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
-// let homeButton = document.getElementById("home");
-// homeButton.onclick = homeOnclick;
-
-// let aboutButton = document.getElementById("about").onclick = () => aboutOnClick();
-// aboutButton.onclick = aboutOnClick;
-
-// let contactButton = document.getElementById("contact");
-// contactButton.onclick = contactOnclick;
-
-// //let projectsContent = document.getElementsByClassName("projects-container");
-
-
-// function homeOnclick() {
-   
-// }
-
-// function aboutOnClick() {
-//     document.getElementById("projectsContent").style.backgroundColor = "red";
-// }
-
-// function contactOnclick() {
-// }
-
 
 
 
